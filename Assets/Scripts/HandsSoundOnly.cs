5e208111a4eb9f8f04f52e9fbba18b6aaea5134f
Jocelyn Weiss <weiss.jocelyn@gmail.com>
2020-07-02 16:37:32 +0200

    Add sound to the sink

---
+++ Assets/Scripts/HandsSoundOnly.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandsSoundOnly : MonoBehaviour
+{
+    public int soundIndex = -1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (soundIndex < 0)
+            return;
+
+        AudioSource audio = TheCellGameMgr.instance.Audio_Bank[soundIndex];
+        if (audio != null)
+        {
+            if (audio.isPlaying == false)
+            {
+                audio.Play();
+            }
+        }
+    }
+}
